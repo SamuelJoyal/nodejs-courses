061806d005acb50ed9000da26516e93012634130
Samuel Joyal <joyal.samuel@carrefour.cegepvicto.ca>
2018-09-22 20:23:08 -0400

    commentaires

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 var app = require('express')(),
     server = require('http').createServer(app),
     io = require('socket.io').listen(server),
-    ent = require('ent'), // Permet de bloquer les caractères HTML (sécurité équivalente à htmlentities en PHP)
+    ent = require('ent'),
     fs = require('fs');
 
-// Chargement de la page index.html
 app.get('/', function (req, res) {
   res.sendfile(__dirname + '/index.html');
 });
